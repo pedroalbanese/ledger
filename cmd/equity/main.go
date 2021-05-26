9a02acc57a6b3372d218208faa74a324d5862cc7
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-05-25 21:13:32 -0300

    Add files via upload

--- cmd/equity/main.go
+++ cmd/equity/main.go
@@ -1,3 +1,4 @@
+//go:generate goversioninfo -manifest=testdata/resource/goversioninfo.exe.manifest
 package main
 
 import (
@@ -8,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"github.com/howeyc/ledger"
 )
 
@@ -17,16 +19,36 @@ const (
 )
 
 func main() {
-
+	var startDate, endDate time.Time
+	startDate = time.Date(1970, 1, 1, 0, 0, 0, 0, time.Local)
+	endDate = time.Now().Add(time.Hour * 24)
+	var startString, endString string
 	var columnWidth int
-
 	var ledgerFileName string
+	var payeeFilter string
 
 	flag.StringVar(&ledgerFileName, "f", "", "Ledger file name (*Required).")
 	flag.IntVar(&columnWidth, "columns", 79, "Set a column width for output.")
+	flag.StringVar(&payeeFilter, "payee", "", "Filter output to payees that contain this string.")
+	flag.StringVar(&startString, "b", startDate.Format(transactionDateFormat), "Begin date of transaction processing.")
+	flag.StringVar(&endString, "e", endDate.Format(transactionDateFormat), "End date of transaction processing.")
 	flag.Parse()
 
-var lreader io.Reader
+	parsedStartDate, tstartErr := time.Parse(transactionDateFormat, startString)
+	parsedEndDate, tendErr := time.Parse(transactionDateFormat, endString)
+
+	if tstartErr != nil || tendErr != nil {
+		fmt.Println("Unable to parse start or end date string argument.")
+		fmt.Println("Expected format: YYYY/MM/dd")
+		return
+	}
+
+	if len(ledgerFileName) == 0 {
+		flag.Usage()
+		return
+	}
+
+	var lreader io.Reader
 
 	if ledgerFileName == "-" {
 		lreader = os.Stdin
@@ -43,6 +65,29 @@ var lreader io.Reader
 	if parseError != nil {
 		fmt.Printf("%s\n", parseError.Error())
 		return
+	}
+
+	timeStartIndex, timeEndIndex := 0, 0
+	for idx := 0; idx < len(generalLedger); idx++ {
+		if generalLedger[idx].Date.After(parsedStartDate) {
+			timeStartIndex = idx
+			break
+		}
+	}
+	for idx := len(generalLedger) - 1; idx >= 0; idx-- {
+		if generalLedger[idx].Date.Before(parsedEndDate) {
+			timeEndIndex = idx
+			break
+		}
+	}
+	generalLedger = generalLedger[timeStartIndex : timeEndIndex+1]
+
+	origLedger := generalLedger
+	generalLedger = make([]*ledger.Transaction, 0)
+	for _, trans := range origLedger {
+		if strings.Contains(trans.Payee, payeeFilter) {
+			generalLedger = append(generalLedger, trans)
+		}
 	}
 
 	var trans ledger.Transaction
