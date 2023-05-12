f6225cfd28ea1c97ad801aa0e1dc8bf3818b3671
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-05-12 16:30:41 -0300

    Add files via upload

---
+++ cmd/linsert/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Parse command line arguments
+	var sourceAccount, targetAccount, amount, payee, note string
+	flag.StringVar(&sourceAccount, "sourceaccount", "", "Source account name")
+	flag.StringVar(&targetAccount, "targetaccount", "", "Target account name")
+	flag.StringVar(&amount, "amount", "", "Transaction amount")
+	flag.StringVar(&payee, "payee", "", "Payee name")
+	flag.StringVar(&note, "note", "", "Transaction note")
+	flag.Parse()
+
+	// Format date
+	date := time.Now().Format("2006/01/02")
+
+	// Print transaction
+	fmt.Printf("; %s\n", note)
+	fmt.Printf("%s %s\n", date, payee)
+	fmt.Printf("    %-65s%10s\n", sourceAccount, fmt.Sprintf("%10s", amount))
+	fmt.Printf("    %-65s%10s\n", targetAccount, fmt.Sprintf("%10s", "-"+amount))
+}
