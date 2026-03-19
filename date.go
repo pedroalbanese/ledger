07754db78fe60672cdf71d62e8cd2b9094ba7d38
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2026-03-19 16:29:36 -0300

    Spellcheck

--- date.go
+++ date.go
@@ -22,7 +22,7 @@ func TransactionsInDateRange(trans []*Transaction, start, end time.Time) []*Tran
 // Period is used to specify the length of a date range or frequency
 type Period string
 
-// Periods suppored by ledger
+// Periods supported by ledger
 const (
 	PeriodWeek     Period = "Weekly"
 	Period2Week    Period = "BiWeekly"
